6ce72ba89db8daa45f86c430713b19f9949c8bb0
LIManuel <leonmalisov@gmail.com>
2020-06-07 18:40:24 -0400

    created login form

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+	state = {
+		email: "",
+		password: ""
+	}
+
+	handleChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		}) 
+	}
+
+	handleSubmit = e => {
+		e.preventDefault()
+		console.log("submitting login info")
+		let rootURL = "http://localhost:3000"
+	}
+
 	render() {
 		return (
 			<div id="login-wrapper">
 				<h1>Login</h1>
+				<form onSubmit={this.handleSubmit}>
+					<label>Email: </label>
+					<input type="email" name="email" onChange={this.handleChange} value={this.state.email}/>
+					<input type="password" name="password" onChange={this.handleChange} value={this.state.password}/>
+					<input type="submit" value="Log in"/>
+				</form>
 			</div>
 		)
 	}
--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -7,10 +7,10 @@ class NavBar extends Component {
 		return (
 			<div id="nav-wrapper">
 				<div id="menu-options">
-					<Link class="nav-link" to="/" >Home</Link>
-					<Link class="nav-link" to="/console" >Console</Link>
-					<Link class="nav-link" to="/review" >Review</Link>
-					<Link class="nav-link" to="/groups" >Groups</Link>								
+					<Link className="nav-link" to="/" >Home</Link>
+					<Link className="nav-link" to="/console" >Console</Link>
+					<Link className="nav-link" to="/review" >Review</Link>
+					<Link className="nav-link" to="/groups" >Groups</Link>								
 				</div>
 				<div id="profile-options">
 					<Link to="/login" >Login</Link>													
