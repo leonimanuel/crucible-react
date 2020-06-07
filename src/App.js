d78a345063b6e64911e98584b7a62b3241da58e4
LIManuel <leonmalisov@gmail.com>
2020-06-07 16:48:57 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+// import { Nav, NavItem, NavLink } from 'reactstrap';
+import NavBar from "./components/navigation/NavBar.js"
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HII</h1>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigation/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class NavBar extends Component {
+	render() {
+		return (
+			<div id="nav-wrapper">
+				<div>Home</div>
+				<div>Console</div>
+				<div>Review</div>
+				<div>Groups</div>				
+			</div>
+		)
+	}
+}
+
+
+export default NavBar;
