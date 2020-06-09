06d8d8557d88b7f68db5357ef9c68582e6c72c23
LIManuel <leonmalisov@gmail.com>
2020-06-09 01:17:16 -0400

    defined rootURL in separate file

--- src/containers/Console.js
+++ src/containers/Console.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
-
+import rootURL from "../rootURL.js"
 
 class Console extends Component {
+	componentDidMount() {
+		let configObj = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json"
+			},
+			body: JSON.stringify({
+				token: localStorage.getItem("token")
+			})
+		}
+		console.log(rootURL())
+		// fetch(rootURL() + `/users/${}`, configObj)
+	}
+
 	render() {
 		return (
 			<div id="console-wrapper">
@@ -10,6 +25,5 @@ class Console extends Component {
 		)
 	}
 }
-
 
 export default Console;
---
+++ src/rootURL.js
@@ -0,0 +1,5 @@
+// export const rootURL = "http://localhost:3000"
+
+export default function rootURL() {
+	return "http://localhost:3000"
+}
