f656a9796af63111dfe4be550b330020b07d9e63
LIManuel <leonmalisov@gmail.com>
2020-06-10 18:00:35 -0400

    started implementing drag and drop, can drag console facts

--- src/components/console/ConsoleFact.js
+++ src/components/console/ConsoleFact.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 // import { connect } from "react-redux"
 
 class ConsoleFact extends Component {
+	startDrag = e => {
+		console.log("dragging")
+		e.dataTransfer.setData("drag-item", this.props.dataItem)
+	}
+
 	render() {
 		return (
-			<div className="fact-box">{this.props.fact.content}</div>
+			<div className="fact-box"
+ 				draggable onDragStart={this.startDrag}>
+				{this.props.fact.content}
+			</div>
 		)
 	}
 }
