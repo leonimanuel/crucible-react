979e5ae2132a1ba7dbdecb01e1d796fa7c5c4d96
LIManuel <leonmalisov@gmail.com>
2020-06-10 18:30:14 -0400

    added highlight on topic drag-over

--- src/components/console/ConsoleTopic.js
+++ src/components/console/ConsoleTopic.js
@@ -3,13 +3,47 @@ import { connect } from "react-redux"
 import { selectTopic } from "../../actions/users.js"
 
 class ConsoleTopic extends Component {
+	state = {
+		draggedOver: false
+	}
+
 	handleTopicSelection = () => {
 		this.props.selectTopic(this.props.topic)
 	}
 
+	allowDrop = e => {
+		e.preventDefault();
+		console.log("allowing drag-over")
+		// e.target.style.back
+	}
+
+	handleDragEnter = e => {
+		// e.preventDefault();
+		this.setState({
+			draggedOver: true
+		})
+		// console.log(e.target.class)
+		// e.target.class.add("blue")
+	}
+
+	handleDragLeave = e => {
+		// e.preventDefault();
+		// e.target.style.backgroundColor = "none"
+		this.setState({
+			draggedOver: false
+		})
+		console.log("aight peace")
+	}
+
 	render() {
 		return (
-			<div onClick={this.handleTopicSelection} className="topic-box">{this.props.topic.name}</div>
+			<div onClick={this.handleTopicSelection} 
+				onDragOver={this.allowDrop} onDragEnter={this.handleDragEnter}
+				onDragLeave={this.handleDragLeave} 
+				className={`topic-box ${this.state.draggedOver ? "dragged-over" : "" }`}
+			>
+				{this.props.topic.name}
+			</div>
 		)
 	}
 }
