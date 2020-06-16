f570e793c874ad8c165691ef38a42bfbfa12c4eb
LIManuel <leonmalisov@gmail.com>
2020-06-16 10:13:36 -0400

    implemented popper for article comments

--- src/components/agora/Article.js
+++ src/components/agora/Article.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux"
 import { fetchDiscussion } from "../../actions/groups.js"
-
+import { createPopper } from "@popperjs/core"
 
 class Article extends Component {
 	state = {
@@ -26,11 +26,21 @@ class Article extends Component {
 		// console.log(e.target === window.getSelection().baseNode.parentNode)
 		if (e.target === window.getSelection().baseNode.parentNode) {
 			let selection= window.getSelection().getRangeAt(0);
+			// console.log(selection)
+			// debugger
 			let selectedText = selection.extractContents();
+			// console.log(selectedText)
 			let span= document.createElement("span");
 			span.style.backgroundColor = "yellow";
+			span.id = `selection-${window.getSelection().anchorOffset}`
 			span.appendChild(selectedText);
 			selection.insertNode(span);
+      
+      const button = document.querySelector(`#${span.id}`);
+      const popup = document.querySelector('#comment-popup');
+			createPopper(button, popup, {
+			  placement: 'bottom',
+			});
 		}
 		// console.log(e.target.getSelection().toString())
 	}
@@ -44,6 +54,7 @@ class Article extends Component {
 					<div>
 						<div id="article-title">{this.props.discussion.article.title}</div>
 						<div onMouseUp={this.handleTextSelect} id="article-content">{this.props.discussion.article.content}</div>						
+						<div id="comment-popup" role="tooltip">My tooltip</div>
 					</div>
 					: 
 					<h3>Loading</h3>}
