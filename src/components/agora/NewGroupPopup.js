7270a75186138ddc3bf3ab0b4a10d66282004bfa
LIManuel <leonmalisov@gmail.com>
2020-06-19 18:10:10 -0400

    memberSuggestions now rendered in popup

--- src/components/agora/NewGroupPopup.js
+++ src/components/agora/NewGroupPopup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux"
 import { fetchUsers } from "../../actions/groups.js"
+import { createPopper } from "@popperjs/core"
 
 import Example from "./Example.js"
 import Autosuggest from 'react-autosuggest';
@@ -14,16 +15,32 @@ class newGroupPopup extends Component {
 		memberSearchVal: ""
 	}
 
+	
 
 	handleChange = (e) => {
 		// debugger
 		this.setState({
+			...this.state,
 			memberSearchVal: e.target.value
 		})
 
 		if (e.target.value) {
 			this.props.fetchUsers(e.target.value)
-		}
+		} 
+	}
+
+	handleFocus = (e) => {
+		e.preventDefault();
+		let button = document.querySelector("#add-member-input");
+		let popup = document.querySelector("#suggestions-popup")
+		popup.style = `width: ${button.clientWidth}px`
+		createPopper(button, popup, {
+		  placement: 'bottom',
+		});		
+	}
+
+	renderSuggestions = () => {
+		return this.props.suggestionMembers.map(member => <MemberSuggestion member={member} />)
 	}
 
 	render() {
@@ -33,12 +50,13 @@ class newGroupPopup extends Component {
 				<div id="new-group-popup-title">New Group</div>
 				<form id="new-group-form">
 					Article link: <input type="text"/> <br/>
-					Members: <input type="text" onChange={this.handleChange} value={this.state.memberSearchVal} />
+					Members: <input id="add-member-input" type="text" onFocus={this.handleFocus} onChange={this.handleChange} value={this.state.memberSearchVal} autocomplete="off" />
+					<div id="added-member-box"></div>
 					Group Name (optional): <input type="text" />
 					<input type="submit" value="Create Group"/>
 				</form>
-				<div id="suggestions-popup">
-					{this.props.suggestionMembers.length > 0 ? this.props.suggestionMembers.map(member => <MemberSuggestion member={member} />) : null}
+				<div id="suggestions-popup" className="popup">
+					{this.props.suggestionMembers.length > 0 ? this.renderSuggestions() : null}
 				</div>
 			</div>
 		)
