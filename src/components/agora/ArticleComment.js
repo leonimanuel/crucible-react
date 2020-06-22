124e443d4da0a9dc2e12bfe14fc538d5f5c5176f
LIManuel <leonmalisov@gmail.com>
2020-06-22 12:08:09 -0400

    renders paragraphs parsed from htmlstring

--- src/components/agora/ArticleComment.js
+++ src/components/agora/ArticleComment.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux"
 
 class ArticleComment extends Component {
-	render() {
+	createContent = () => {
+		let parser = new DOMParser();
+		let doc = parser.parseFromString(this.props.content, "text/html")
+		let pCollection = doc.getElementsByTagName("p")
+		let pArray	= Array.from(pCollection)
 		// debugger
-		const { match } = this.props;
+		let pTextArray = pArray.map(p => <p>{p.innerText}</p>)
+		return pTextArray
+	}
+
+	render() {
 		return (
 			<div id={this.props.id}>
-				{this.props.comment.content}
+				{this.createContent()}
 			</div>
 		)
 	}
 }
 
 
-export default ArticleComment;
+export default connect(state => ({content: state.discussion.discussion.article.content}))(ArticleComment);
