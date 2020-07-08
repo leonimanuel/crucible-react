becc6f79c3c31514c480e968abf9a1201c29b670
LIManuel <leonmalisov@gmail.com>
2020-07-08 12:25:06 -0400

    homepage rewrite and styling

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -12,7 +12,8 @@ class Home extends Component {
 	render() {
 		return (
 			<div onMouseUp={this.handleMouseUp} id="home-wrapper">
-				<h1>This is the home page. isn't it nice?</h1>
+				<h1>Welcome to Crucible</h1>
+				<h4>Crucible is a place for organized conversations about the day's headlines where facts come first. To get started, create a new group in the Agora section of the side-panel</h4>
 			</div>
 		)
 	}
