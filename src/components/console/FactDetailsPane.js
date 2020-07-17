dbdaa3ba5994851a06e0262b69df4fe7364cb978
LIManuel <leonmalisov@gmail.com>
2020-07-17 10:57:51 -0400

    shows all review types upvotes and downvotes

--- src/components/console/FactDetailsPane.js
+++ src/components/console/FactDetailsPane.js
@@ -4,8 +4,13 @@ import React, { Component } from 'react';
 class FactDetailsPane extends Component {
 	render() {
 		return (
-			<div>
-				DETAILS
+			<div className="fact-details-pane">
+				<div>Logic upvotes: {this.props.fact.logic_upvotes}</div>
+				<div>Logic downvotes: {this.props.fact.logic_downvotes}</div>
+				<div>Context upvotes: {this.props.fact.context_upvotes}</div>
+				<div>Context downvotes: {this.props.fact.context_downvotes}</div>
+				<div>Credibility upvotes: {this.props.fact.credibility_upvotes}</div>
+				<div>Credibility downvotes: {this.props.fact.credibility_downvotes}</div>
 			</div>
 		)
 	}
