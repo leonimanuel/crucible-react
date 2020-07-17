f84cf67067097cccea3ca73093dd4c62d748ddb2
LIManuel <leonmalisov@gmail.com>
2020-07-17 10:41:07 -0400

    clicking factbox toggles fact details pane

--- src/components/console/ConsoleFact.js
+++ src/components/console/ConsoleFact.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react';
 // import { connect } from "react-redux"
 
 class ConsoleFact extends Component {
+	state = {
+		showFactDetails: false
+	}
+
 	startDrag = e => {
 		console.log("dragging")
 		e.dataTransfer.setData("object", JSON.stringify({parentTopic: this.props.parentTopic, fact: this.props.fact}))
 	}
 
+	toggleFactDetailPane = () => {
+		this.setState({showFactDetails: !this.state.showFactDetails})
+	}
+				 // <FactDetailPane />
+
 	render() {
 		return (
-			<div id={`${this.props.parentTopic.name}-fact-${this.props.fact.id}`} className="fact-box"
- 				draggable onDragStart={this.startDrag}>
-				{this.props.fact.content}
+			<div>
+				<div 
+					id={`${this.props.parentTopic.name}-fact-${this.props.fact.id}`} 
+					className="fact-box"
+	 				draggable 
+	 				onDragStart={this.startDrag}
+	 				onClick={this.toggleFactDetailPane}
+				>
+						{this.props.fact.content}
+				</div>				
+			
+				{this.state.showFactDetails ? <div>DETAILS</div> : null}
 			</div>
 		)
 	}
