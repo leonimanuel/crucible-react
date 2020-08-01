3ee38a3145a3c4ef29b11ccd40737c2bfca9fdfa
LIManuel <leonmalisov@gmail.com>
2020-08-01 18:48:57 -0400

    implemented basic design and logic for daily_reviews bar

--- src/components/home/ScoresOverview.js
+++ src/components/home/ScoresOverview.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { connect } from "react-redux"
 
 class ScoresOverview extends Component {
+	componentDidMount() {
+		debugger
+		let scoreBar = document.getElementById("shadow-daily-reviews-bar");
+		if (this.props.dailyReviews < 10) {
+			scoreBar.style.width = `${this.props.dailyReviews * 10}px`
+		} else {
+			scoreBar.style.width = "100px"
+		}
+		
+	}
 
 	render() {
 		return (
@@ -9,7 +19,24 @@ class ScoresOverview extends Component {
 				<div className="overview-header" id="scores-overview-header">Status</div>
 				<div className="overview-content-container" id="overview-scores-container">
 					<div>{this.props.reputabilityScore}</div>
-					<div>{this.props.reviewScore}</div>
+					<div>
+						<div>Reviews</div>
+						<div id="daily-reviews-bar">
+							<div className="daily-reviews-notch"></div>
+							<div className="daily-reviews-notch"></div>
+							<div className="daily-reviews-notch"></div>
+							<div className="daily-reviews-notch"></div>
+							<div className="daily-reviews-notch"></div>
+							<div className="daily-reviews-notch"></div>
+							<div className="daily-reviews-notch"></div>
+							<div className="daily-reviews-notch"></div>
+							<div className="daily-reviews-notch"></div>
+							<div className="daily-reviews-notch"></div>
+						
+							<div id="shadow-daily-reviews-bar"></div>
+						</div>
+
+					</div>
 				</div>
 			</div>			
 		)
@@ -19,7 +46,8 @@ class ScoresOverview extends Component {
 const mapStateToProps = state => {
 	return {
 		reputabilityScore: state.users.userReputabilityScore,
-		reviewScore: state.users.userReviewScore
+		reviewScore: state.users.userReviewScore,
+		dailyReviews: state.users.user.daily_reviews
 	}
 }
 
