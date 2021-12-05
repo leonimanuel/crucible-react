7f86a01fe5edb9ab629093f093f5207f6b22380c
LIManuel <leonmalisov@gmail.com>
2021-12-05 16:22:11 -0500

    added Timeline component, rendering followed content (facts only right now)

---
+++ src/containers/Timeline.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Route, withRouter, Redirect } from "react-router-dom";
+import { useLastLocation } from 'react-router-last-location';
+import { connect } from "react-redux"
+import { v4 as uuidv4 } from 'uuid';
+import { API_ROOT } from "../constants"
+
+import SupportingFact from "../components/agora/SupportingFact.js"
+// import { ActionCable } from "react-actioncable-provider";
+// import { API_ROOT } from "../constants"
+import "./timeline.css"
+
+class Timeline extends Component {
+	state = {
+		timeline_activities: []
+	}
+
+	showTimelineItem = (activity) => {
+		debugger
+		switch (activity.item.type) {
+			case "fact":
+				return (
+					// <div>{activity.item.content}</div>
+					<SupportingFact fact={activity.item.object}/>
+				)
+
+			default:
+				return <div>Item type not found</div>
+		}
+
+	}
+
+	componentDidMount() {
+		let configObj = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				Authorization: localStorage.getItem("token")
+			}
+		}
+
+		fetch(API_ROOT + "/timeline", configObj)
+			.then(resp => resp.json())
+			.then(activities => {
+				this.setState({timeline_activities: activities})
+			})			
+	}
+
+	render() {
+		return (
+			<div id="timeline-wrapper">
+				{this.state.timeline_activities.map(activity => this.showTimelineItem(activity))}
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+	}
+}
+
+
+
+export default withRouter(connect(mapStateToProps)(Timeline));
+
+
+
+
