e91f4b14bd66c05b10feb74bf7cbfac6907ca255
LIManuel <leonmalisov@gmail.com>
2021-12-11 18:54:54 -0500

    added add fact button to TimelineFact

--- src/components/timeline/TimelineFact.js
+++ src/components/timeline/TimelineFact.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import parse from 'html-react-parser';
+import { connect } from "react-redux"
+import { addFactFromComment } from "../../actions/discussionsActions.js"
+
 import TimelineItemHeader from "./TimelineItemHeader.js"
 
 class TimelineFact extends Component {
+	handleAddFact = (fact) => {
+		console.log("executing handleAddFact")
+		// debugger
+		this.props.addFactFromComment(fact, fact.collector_id);
+	}	
+
 	render() {
 		const { fact } = this.props;
 		
@@ -16,16 +25,31 @@ class TimelineFact extends Component {
 		// 		border = "3px solid red" 		
 		// }
 		return (
-			<div className="timeline-fact timeline-item" style={{border: border}}>
-				{parse(innerHTML)}
-			</div>
+			<div className="timeline-fact timeline-item">
+				{this.props.userId !== fact.user_id && !this.props.userFacts.find(f => f.id === fact.id)
+					? 
+						<button 
+							className="add-comment-fact-button" 
+							onClick={() => this.handleAddFact(fact)}
+						>+</button>
+					: 
+						null
+				}
+
+				<div className="timeline-fact timeline-item" style={{border: border}}>
+					{/*parse(innerHTML)*/}
+					{fact.content}
+				</div>
+			</div>			
 		)
 	}
 }
 
+const mapStateToProps = state => {
+	return {
+		userId: state.users.userId,
+		userFacts: state.topics.facts 
+	}
+}
 
-export default TimelineFact;
-
-
-
-
+export default connect(mapStateToProps, { addFactFromComment })(TimelineFact);
