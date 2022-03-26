4fb6c81a93dd785b6dcf44f9f6e8b964df611a32
LIManuel <leonmalisov@gmail.com>
2022-03-26 19:56:29 -0400

    hide long reply chains

--- src/components/timeline/RepliesContainer.js
+++ src/components/timeline/RepliesContainer.js
@@ -7,16 +7,29 @@ import ReplyForm from "./ReplyForm.js"
 
 const RepliesContainer = (props) => {
 	const [stateShowReplyForm, setStateShowReplyForm] = useState(false);
+	const [showAllReplies, setShowAllReplies] = useState(false);
 
 	const { comment } = props
+
+	const visibleReplies = showAllReplies ? props.replies : props.replies.filter((reply, index) => { return index >= (props.replies.length - 2)})
+
 	return (
 		<div className="replies-container">
 			{ props.replies.length 
 				?
 					<React.Fragment>
 						{props.replies.length ? <div className="replies-header">Replies</div> : null}
+						
+						{
+							(showAllReplies || visibleReplies.length == props.replies.length) ? null : 
+							<div className="replies-viewtoggle" onClick={() => setShowAllReplies(true)}>
+								show all {props.replies.length} replies
+							</div>
+						}
+
+
 						<div className="replies-wrapper" key={comment.id}>
-							{props.replies.map(reply => <Reply comment={comment} reply={reply}/>)}
+							{visibleReplies.map(reply => <Reply comment={comment} reply={reply}/>)}
 						</div>
 					</React.Fragment>
 				: null
