420f7c163a4a1e25d2b7b1ef915866c68b550f35
LIManuel <leonmalisov@gmail.com>
2022-04-11 11:59:29 +0100

    implement 'back' function on topic navigation

--- src/components/console/ConsoleWindow.js
+++ src/components/console/ConsoleWindow.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux"
+
+import { selectTopic } from "../../actions/topicsActions.js"
+
 import ConsoleTopicsList from "./ConsoleTopicsList.js"
 import ConsoleFactsList from "./ConsoleFactsList.js"
 
@@ -8,11 +11,15 @@ class ConsoleWindow extends Component {
 		// debugger
 	}
 
+
 	render() {
 		console.log("rendering console window")
+		
+		const currentTopicParent = this.props.topics.find(t => t.id == this.props.parentTopic.parent_id)
+
 		return (
 			<div id="console-window">
-				{this.props.parentTopic.parent_id ? <div id="topic-back-button">⬅ {this.props.topics.find(t => t.id == this.props.parentTopic.parent_id).name}</div> : null }
+				{this.props.parentTopic.parent_id ? <div id="topic-back-button" onClick={() => this.props.selectTopic(currentTopicParent)}>⬅ {currentTopicParent.name}</div> : null }
 				<div id="console-window-title">{this.props.parentTopic ? this.props.parentTopic.name : "NONE"}</div>
 						
 					{this.props.parentTopic.name === "New Facts" ? null 
@@ -33,7 +40,7 @@ const mapStateToProps = state => {
 	}
 }
 
-export default connect(mapStateToProps)(ConsoleWindow);
+export default connect(mapStateToProps, { selectTopic })(ConsoleWindow);
 
 
 
