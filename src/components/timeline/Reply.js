6e65bb6213dc19c5b044cadf22d8b8985d7027e9
LIManuel <leonmalisov@gmail.com>
2022-04-16 11:30:40 -0400

    add reply timestamps

--- src/components/timeline/Reply.js
+++ src/components/timeline/Reply.js
@@ -6,7 +6,11 @@ import { addFactFromComment } from "../../actions/discussionsActions.js"
 import SupportingFact from "../agora/SupportingFact.js"
 import TaggedUsers from "./TaggedUsers.js"
 
+import Moment from 'react-moment';
+import moment from 'moment-timezone';
+
 const Reply = (props) => {
+	moment.tz.setDefault("UTC") // VERY IMPORTANT. Otherwise, moment will think the activity timestamp is local timezone
 
 	const handleAddFact = (fact) => {
 		props.addFactFromComment(fact, props.message.comment.user_id);
@@ -22,9 +26,15 @@ const Reply = (props) => {
 
 		reply.reply_comment_facts = sortedCommentFacts
 	}
+	
+	debugger
 	return (
 		<div className="reply-container">
-			<div className="reply-user-handle">{reply.user.handle ? reply.user.handle : "unavailable"}</div> 
+			<div className="reply-header">
+				<div className="reply-user-handle">{reply.user.handle ? reply.user.handle : "unavailable"}</div>
+				{reply.created_at ? <Moment className="reply-timestamp" fromNow>{reply.created_at}</Moment> : null}
+			</div>
+
 
 			{!reply.reply_comment
 				?
