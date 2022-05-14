bde528dd45961be14f61e93fc75533cc54de683c
LIManuel <leonmalisov@gmail.com>
2022-05-14 17:08:43 -0400

    restyle notifications

--- src/components/notifications/Notification.js
+++ src/components/notifications/Notification.js
@@ -11,27 +11,40 @@ const Notification = (props) => {
 	const additional_actor_count = notification_group.actor_count - 1
 	const notificationObject = notification_group.group_object
 	const generateNotificationText = () => {
+		let notifTypeClassName = ""
+		let notifDescription = ""
+
 		switch (notification_group.action_type) {
 			case "add_position":
-				return (
-					<span><Link className="contact-result-link" to={`/profiles/${latest_actor.id}`} >{latest_actor.handle}</Link> created a new position</span> 							
-				)
+				notifTypeClassName = "add_position"
+				notifDescription = "created a new position"
+				break
 
 			case "add_comment":
-				return (
-					<span><Link className="contact-result-link" to={`/profiles/${latest_actor.id}`} >{latest_actor.handle}</Link>  tagged you on a post:</span> 											
-				)
+				notifTypeClassName = "add_comment"
+				notifDescription = " tagged you on a post:"				
+				break
 
 			case "create_reply":
-				return (
-					<span><Link className="contact-result-link" to={`/profiles/${latest_actor.id}`} >{latest_actor.handle}</Link> {additional_actor_count ? `and ${additional_actor_count} others` : ""} replied to your comment: </span> 	
-				)		
+				notifTypeClassName = "create_reply"
+				notifDescription =  `${additional_actor_count ? `and ${additional_actor_count} others` : ""} replied to your comment: `
+				break
 
 			case "tag_user_on_reply":
-				return (
-					<span><Link className="contact-result-link" to={`/profiles/${latest_actor.id}`} >{latest_actor.handle}</Link>  tagged you on a reply: </span> 												
-				)
+				notifTypeClassName = "tag_user_on_reply"
+				notifDescription =  "  tagged you on a reply: "
+				break
+
+			case "add_article_share":
+				debugger
+				notifTypeClassName = "add_article_share"
+				notifDescription =  " shared an article: "
+				break
+
+			default: debugger
 		}
+
+		return <span className={`notification-description ${notifTypeClassName}`}><Link className="contact-result-link" to={`/profiles/${latest_actor.id}`} >{latest_actor.handle}</Link>{notifDescription}</span>
 	}
 
 	if (!notificationObject) {
@@ -44,7 +57,7 @@ const Notification = (props) => {
 				{generateNotificationText()}
 
 				<span className="notification-target" >
-					{notificationObject.content}			
+					{notificationObject.content}		
 				</span> 			
 			</Link>			 
 		</div>
