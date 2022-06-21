35d01c1ed9b77b582274207d2a8f1e8dc3d5bab1
LIManuel <leonmalisov@gmail.com>
2022-06-20 20:09:08 -0400

    changed FactDropZone a bit

---
+++ src/components/timeline/ResponseExcerptDropzone.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { generateContext, handleArticleClick } from "../../helpers/helpers.js"
+
+import SupportingChatFact from "../social/SupportingChatFact.js"
+import parse from "html-react-parser";
+
+class ResponseExcerptDropzone extends Component {
+  state = {
+    draggedOver: false
+  }
+
+  allowDrop = e => {
+    e.preventDefault();
+    // this.setState({draggedOver: true})
+    e.target.dataset.dragged_over = true
+  }
+
+  handleDragEnter = e => {
+    e.persist();
+    // this.setState({draggedOver: true})
+    e.target.dataset.dragged_over = true
+  }
+
+  handleDragLeave = e => {
+    e.persist();
+    // this.setState({draggedOver: false})
+    e.target.dataset.dragged_over = false
+  }
+
+  drop = e => {
+    e.preventDefault();
+    // this.setState({draggedOver: false})
+    e.target.dataset.dragged_over = false
+    // console.log(JSON.parse(e.dataTransfer.getData("object")))
+    let transferObj = JSON.parse(e.dataTransfer.getData("object"))
+    e.persist()
+
+    let draggedFact = transferObj.fact    
+    this.props.handleResponseExcerptUpdate(draggedFact)
+  }	
+
+  handleRemoveExcerpt = () => { 
+    this.props.handleResponseExcerptRemoval()
+  }
+
+	render() {
+		const excerpt = this.props.responseExcerpt
+
+    return (
+      <React.Fragment>
+        {
+          excerpt 
+            ?
+          <div className="timeline-comment-context-wrapper">
+            <div style={{"margin-top": "10px"}}>
+              <a 
+                className="article-anchor reply-excerpt" 
+                href={excerpt.article_url} 
+                onClick={(e, resoure) => handleArticleClick(e, excerpt)}
+              >
+                {excerpt.article_title || excerpt.article_url.split("/")[2].replace("www.", "")}
+              </a>
+            </div> 
+
+            <div className="timeline-comment-context-bubble">
+              {excerpt.node_text ? <div className="timeline-comment-context">...{parse(generateContext(excerpt))}...</div> : <div className="timeline-comment-context">{excerpt.content}</div>}
+              <div className="remove-fact-button" onClick={this.handleRemoveExcerpt}>✕</div>
+            </div>
+          </div>
+            :
+          <div className="chat-fact-dropzone-bubble">          
+            <div className="chat-fact-dropzone">
+              {this.props.placeholder}
+              <div className="comment-facts-container">
+                <div
+                  className="fact-dropslot"
+                  data-preceding_fact_id="first"
+                  data-dragged_over="false"
+                  onDragOver={this.allowDrop} 
+                  onDragEnter={this.handleDragEnter}
+                  onDragLeave={this.handleDragLeave}
+                  onDrop={this.drop}
+                  style={{display: this.props.dropType == "responseExcerpt" && this.props.facts.length ? "none" : "block"}}  
+                >
+                  {/*"Drag First Fact Here"*/}
+                </div>
+              </div>                
+            </div> 
+          </div>
+        }          
+      </React.Fragment>
+		)
+	}
+}
+
+export default ResponseExcerptDropzone;
---
+++ src/components/timeline/SupportingExcerpt.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+import Moment from 'react-moment';
+import moment from 'moment-timezone';
+import parse from 'html-react-parser';
+
+import { generateContext, handleArticleClick } from "../../helpers/helpers.js"
+
+// This component is specifically for facts in the Chat Drop Zone
+class SupportingExcerpt extends Component {
+	state = {
+		factPosition: "",
+		showOriginalFact: false
+	}
+
+	startDrag = e => {
+		console.log("dragging")
+		e.dataTransfer.setData("object", JSON.stringify({fact: this.props.fact, origin: "dropzone"}))
+		this.setState({factPosition: this.props.facts.indexOf(this.props.fact)})
+		
+		// e.persist()
+		// setTimeout(() => {
+		// 	e.target.style.visibility = "hidden"
+		// }, 500)
+		// setTimeout(() => this.props.handleDrag(this.props.facts.filter(f => f.id != this.props.fact.id)), 200)
+		
+	}
+
+	endDrag = (e) => {
+		// this.props.handleDrag(this.props.facts.splice((this.state.factPosition - 1), 0, this.props.fact))
+		// alert("I just got dropped")
+		// e.target.style.height = "50px"
+		// e.target.style.display = "block"
+	}
+
+	toggleFactRephrase = () => this.setState({showOriginalFact: !this.state.showOriginalFact})
+
+	render() {
+		const { fact } = this.props;
+		let border
+		// if (fact.review_status === "pending") {
+		// 	border = "3px solid #ff9234" //yellow
+		// } else if (fact.review_status === "pass") {
+		// 		border = "3px solid green" 		
+		// } else {
+		// 		border = "3px solid red" 		
+		// }
+		return (
+			<div 
+				className="timeline-fact-wrapper"
+ 				draggable 
+ 				onDragStart={this.startDrag}			
+ 				onDragEnd={this.endDrag}					
+			>
+				<div className="timeline-fact-options-wrapper">
+					<div className="show-context-indicator timeline-fact-option" onClick={this.toggleFactContext}> {this.state.showContext ? "hide" : "show"} context</div>	
+					<div className="show-context-indicator timeline-fact-option" onClick={() => window.open(fact.url,'_blank')}> source</div>	
+					{!this.state.showContext && !!fact.rephrase ? <div className="original-vs-rephrase-indicator timeline-fact-option" onClick={this.toggleFactRephrase}> show {this.state.showOriginalFact ? "rephrase" : "original"} </div> : null}					
+				</div>
+
+				{
+					this.state.showContext 
+					?
+					<div className="timeline-fact-context-wrapper">
+						<div className="timeline-fact-context">{parse(generateContext(fact))}</div>
+						{/*<div className="context-lip"></div>*/}
+					</div>
+					:
+					<div className="timeline-fact-content" style={{border: border}}>
+						{/*parse(innerHTML)*/}
+						{/*fact.content*/}
+						{fact.rephrase ? (this.state.showOriginalFact ? fact.content : fact.rephrase.content) : fact.content}
+					</div>					
+				}
+				<a className="fact-source" href={fact.url} target="_blank">- {fact.url.split("/")[2].replace("www.", "")}</a>
+				<div className="fact-collection-timestamp">collected&nbsp;{<Moment fromNow>{fact.created_at}</Moment>}</div>
+				<div className="remove-fact-button" onClick={() => this.props.sendRemoval(fact.id)}>✕</div>
+			</div>		
+		)
+	}
+}
+
+
+export default SupportingExcerpt;
+
+
+
+
